8fb8d60bea25810fe5d258eec2ec151240bbe721
Sufeng Niu <sniu@g.clemson.edu>
2019-03-07 13:24:12 -0500

    update to latest

--- sh/run_pointnet_multi.sh
+++ sh/run_pointnet_multi.sh
@@ -5,12 +5,12 @@ TOP_PATH=/scratch4/sniu/pc
 
 # pointnet
 python main.py --loss_type=emd --save_path=${TOP_PATH}/pointnet_multi_emd \
-               --fb_split=False --PW=32,16 --PL=32,16 --cell_max_points=512,512 \
-               --cell_min_points=50,50 --encoder=pointnet --decoder=pointnet \
-               --stacked=True --rotation=True --pooling=mean --training=True
+               --fb_split=False --PW=32 --PL=32 --cell_max_points=512 \
+               --cell_min_points=50 --encoder=pointnet --decoder=pointnet \
+               --stacked=False --rotation=False --pooling=max --training=True --num_epochs=281
 
 # evaluation
 python main.py --loss_type=emd --save_path=${TOP_PATH}/pointnet_multi_emd \
-               --fb_split=False --PW=32,16 --PL=32,16 --cell_max_points=512,512 \
-               --cell_min_points=50,50 --encoder=pointnet --decoder=pointnet \
-               --stacked=True --rotation=True --pooling=mean --training=False
+               --fb_split=False --PW=32 --PL=32 --cell_max_points=512 \
+               --cell_min_points=50 --encoder=pointnet --decoder=pointnet \
+               --stacked=False --rotation=False --pooling=max --training=False --weights="model-280.ckpt"
