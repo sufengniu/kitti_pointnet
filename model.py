fix the multi-scale compression ratio calculation

model.py
@@ -759,7 +759,7 @@ class AutoEncoder():
                                                          np.array(save_var_part).mean(), 
                                                          np.array(save_mse_part).mean()))
 
-        cell_rate, sweep_rate = point_cell.test_compression_rate()
+        cell_rate, sweep_rate = point_cell.test_compression_rate(level_idx=level_idx)
 
         emd_all = ['emd_all', np.array(save_emd_all).mean()]
         chamfer_all = ['chamfer_all', np.array(save_chamfer_all).mean()]

util.py
@@ -956,7 +956,7 @@ class LoadData():
             multi_cell[k] = multi_cell[k].dropna(thresh=5)
         return multi_cell
 
-    def test_compression_rate(self):
+    def test_compression_rate(self, level_idx=0):
 
         print ('calculating average testing compression rate ...')
         cell_rate_record, sweep_rate_record = [], []
@@ -965,7 +965,7 @@ class LoadData():
                 sweep_cell = self.partition_single_range(sweep, i)
             else:
                 sweep_cell = self.partition_single(sweep, i)
-            cell_rate, sweep_rate = self.calculate_compression_rate(sweep_cell[0]) # only calculate compression on the finest scale
+            cell_rate, sweep_rate = self.calculate_compression_rate(sweep_cell[level_idx]) # only calculate compression on the finest scale
             cell_rate_record.append(cell_rate)
             sweep_rate_record.append(sweep_rate)
         print ('average cell compression rate: %.6f' % (np.mean(cell_rate_record)))