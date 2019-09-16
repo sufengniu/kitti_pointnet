ef7975ab5ea7cebd54856163a528de53d1b3f886
Sufeng Niu <sniu@g.clemson.edu>
2019-09-16 01:21:59 -0400

    upload external lib

---
+++ external/structural_losses/tf_nndistance_compile.sh
@@ -0,0 +1,1 @@
+nvcc -std=c++11 -c -o tf_nndistance_g.cu.o tf_nndistance_g.cu -I /home/sniu/tensorflow/lib/python3.4/site-packages/tensorflow/include -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -O2 && g++ -std=c++11 tf_nndistance.cpp tf_nndistance_g.cu.o -o tf_nndistance_so.so -shared -fPIC -I /home/sniu/tensorflow/lib/python3.4/site-packages/tensorflow/include -O2
